da6fa4fb6fabea7f293c711c5e49efa951ad6a9c
장대석 <wkd4914@naver.com>
2020-07-01 23:58:54 +0900

    첫번째커밋

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,16 @@
+package com.cos.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//스프링이 com.cos.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 NEW 하는건 아니고,
+//특정 어노테이션이 붙어 있는 클래스 파일들을 new 해서(IOC) 스프링 컨테이너에 관리해준다.
+@RestController 
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>헬로우 스프링 부터</h1>";
+	}
+}
